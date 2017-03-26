9591334666063ebe5bb234b18c96bd0953192486
ceq <chenenqiang1234@163.com>
2017-03-26 16:21:27 +0800

    b

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         int f;
+        int ff;
         setContentView(R.layout.activity_main);
         String b;
     }
